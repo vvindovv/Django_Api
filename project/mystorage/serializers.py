34bb64fa7a4510d16c54af15c4b2e52d3dd235bf
sangen <tkddjs5@naver.com>
2019-10-31 05:45:43 +0900

    final

---
+++ project/mystorage/serializers.py
@@ -0,0 +1,27 @@
+from .models import Essay, Album, Files
+from rest_framework import serializers
+from rest_framework.parsers import MultiPartParser, FormParser
+
+class EssaySerializer(serializers.ModelSerializer):
+    
+    author_name =  serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Essay
+        fields = ('pk', 'title', 'body', 'author_name')
+
+class AlbumSerializer(serializers.ModelSerializer):
+    
+    author_name =  serializers.ReadOnlyField(source='author.username')
+    image = serializers.ImageField(use_url=True)
+    class Meta:
+        model = Album
+        fields = ('pk', 'author_name', 'image', 'description')
+
+class FilesSerializer(serializers.ModelSerializer):
+    
+    author_name =  serializers.ReadOnlyField(source='author.username')
+    myfile = serializers.FileField(use_url=True)
+    class Meta:
+        model = Files
+        fields =('pk', 'author_name', 'myfile', 'description')
